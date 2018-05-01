7c615cedc5dbe5b2c657b8b964e40fbc68e765e8
rob-kistner <dev@robkistner.com>
2018-05-01 15:51:37 -0500

    3-16

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,86 +1,47 @@
 'use strict';
 
-/**
- * CHALLENGE
- * 
- * 1. only render the subtitle and p tag if subtitle exists - use logical && operator
- * 2. conditionally render new p tag if options.length > 0, return 'Here are your options...'
-or return 'no options'
- */
+/* -- COURSE COMPONENT -------------------------------------- */
 
-var appinfo = {
-    title: 'My Title',
-    subtitle: 'This is the subtitle',
-    options: ['One', 'Two']
+var count = 0;
+
+var addOne = function addOne() {
+    console.log('addOne');
+};
+var minusOne = function minusOne() {
+    console.log('minusOne');
+};
+var reset = function reset() {
+    console.log('reset');
 };
 
-var tmplApp = React.createElement(
+var tmplCounter = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        appinfo.title
-    ),
-    appinfo.subtitle && React.createElement(
-        'p',
-        null,
-        appinfo.subtitle
+        'Count: ',
+        count
     ),
     React.createElement(
-        'p',
-        null,
-        appinfo.options && appinfo.options.length > 0 ? 'Here are your options...' : 'No options available'
+        'button',
+        { onClick: addOne },
+        '+1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: minusOne },
+        '-1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: reset },
+        'reset'
     )
 );
 
-var user = {
-    name: 'Rob Kistner',
-    age: 48,
-    location: 'Chicago'
-
-    /**
-     * Note the use of JSX within the expression
-     * to insert the value conditionally
-     * 
-     * @param {string} location 
-     */
-};function getLocation(location) {
-    if (location) {
-        return React.createElement(
-            'p',
-            null,
-            'Location: ',
-            location
-        );
-    }
-    return undefined;
-}
-
-/**
- * Turnary operator for conditional user name
- * 
- * For user.age:
- * Remember that && will ignore the 2nd statement 
- * if the first statement is falsy
- */
-var tmplUser = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name ? user.name : 'Anonymous'
-    ),
-    user.age && user.age >= 18 && React.createElement(
-        'p',
-        null,
-        'Age: ',
-        user.age
-    ),
-    getLocation(user.location)
-);
+/* -- RENDER -------------------------------------- */
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(tmplApp, appRoot);
+ReactDOM.render(tmplCounter, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,66 +1,31 @@
-/**
- * CHALLENGE
- * 
- * 1. only render the subtitle and p tag if subtitle exists - use logical && operator
- * 2. conditionally render new p tag if options.length > 0, return 'Here are your options...'
-or return 'no options'
- */
+/* -- COURSE COMPONENT -------------------------------------- */
 
-let appinfo = {
-    title: 'My Title',
-    subtitle: 'This is the subtitle',
-    options: ['One', 'Two']
+let count = 0;
+
+const addOne = () => {
+    console.log('addOne');
+};
+const minusOne = () => {
+    console.log('minusOne');
+};
+const reset = () => {
+    console.log('reset');
 };
 
-let tmplApp = (
-
+const tmplCounter = (
     <div>
-        <h1>{ appinfo.title }</h1>
-        { appinfo.subtitle && <p>{appinfo.subtitle}</p> }
-        <p>{ (appinfo.options && appinfo.options.length > 0) ? 'Here are your options...' : 'No options available' }</p>
+        <h1>Count: { count }</h1>
+        <button onClick={ addOne }>+1</button>
+        <button onClick={ minusOne }>-1</button>
+        <button onClick={ reset }>reset</button>
     </div>
-
 );
 
-let user = {
-    name: 'Rob Kistner',
-    age: 48,
-    location: 'Chicago'
-}
-
-/**
- * Note the use of JSX within the expression
- * to insert the value conditionally
- * 
- * @param {string} location 
- */
-function getLocation( location ) {
-    if(location) {
-        return <p>Location: {location}</p>
-    }
-    return undefined;
-}
-
-/**
- * Turnary operator for conditional user name
- * 
- * For user.age:
- * Remember that && will ignore the 2nd statement 
- * if the first statement is falsy
- */
-let tmplUser = (
-
-    <div>
-        <h1>{ user.name ? user.name : 'Anonymous' }</h1>
-        { (user.age && user.age >= 18) && <p>Age: { user.age}</p>}
-        { getLocation(user.location) }
-    </div>
-
-);
+/* -- RENDER -------------------------------------- */
 
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(
-    tmplApp,
+    tmplCounter,
     appRoot
 );
