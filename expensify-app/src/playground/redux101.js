382b17ab515d25dda87561ed7f80d7d51e9c4b6d
rob-kistner <dev@robkistner.com>
2018-05-15 14:30:11 -0500

    87 - Subscribing and Dynamic Actions

--- expensify-app/src/playground/redux101.js
+++ expensify-app/src/playground/redux101.js
@@ -2,52 +2,73 @@ import React, {Component} from 'react';
 import { createStore } from 'redux';
 
 
-/**
- * 
-NOTE: Installed 3.7.2 of Redux 
-in case its just not working
-
- * 
- * initializing state on createStore as the parameter
- * to the anonymous function
- * 
- * second argument is the action itself
- * 
- */
 const store = createStore( ( state = { count: 0 }, action ) => {
   switch(action.type) {
+
     case 'INCREMENT':
+      const incrementBy = (typeof action.incrementBy === 'number') ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       }
+
     case 'DECREMENT':
+    const decrementBy = (typeof action.decrementBy === 'number') ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
       }
-    case 'RESET':
+      
+    case 'SET':
+      return {
+        count: action.count
+      }
+
+      case 'RESET':
       return {
         count: 0
       }
+
     default:
       return state;
+
   }
 });
 
 
 /**
- * using Actions to dispatch...
- * Increment using Actions
- * Decrement using Actions
- * Reset using Actions
+ * called the subscription after attaching it to a variable,
+ * in this case 'unsubscribe', will remove the subscription
  */
-store.dispatch({
-  type: 'INCREMENT'
+const unsubscribe = store.subscribe(()=>{
+  console.log(store.getState());
 });
+
+
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'INCREMENT',
+  incrementBy: 5
 });
+
 store.dispatch({
   type: 'INCREMENT'
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+});
+
+store.dispatch({
+  type: 'DECREMENT'
+});
+
+store.dispatch({
+  type: 'RESET'
+});
+
+store.dispatch({
+  type: 'SET',
+  count: 101
+})
+
+// remove subscription now!
+unsubscribe();
